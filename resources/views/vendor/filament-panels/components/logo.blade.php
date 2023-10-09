e903c1e745f4dd0c01c5ade216ed00059db190e7
Evren Ceyhan <evren.ceyhan@gmail.com>
2023-10-09 22:45:28 +0200

    asset path refactored

--- resources/views/vendor/filament-panels/components/logo.blade.php
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -1,6 +1,6 @@
 <div class="flex items-center gap-4">
 
-    <img src="img/logo.svg" class="h-10" alt="Filament Demo" />
+    <img src="{{ asset('img/logo.svg') }}" class="h-10" alt="Filament Demo" />
 
     <h1 class="text-2xl text-gray-400">
         {{ env('APP_NAME') }}
