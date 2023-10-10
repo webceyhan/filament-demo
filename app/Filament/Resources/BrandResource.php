e241c9ba8885e54a4a3d681dbdb346503b1a9456
Evren Ceyhan <evren.ceyhan@gmail.com>
2023-10-10 23:16:00 +0200

    brand table colum actions added

--- app/Filament/Resources/BrandResource.php
+++ app/Filament/Resources/BrandResource.php
@@ -93,7 +93,11 @@ class BrandResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                Tables\Actions\ActionGroup::make([
+                    Tables\Actions\ViewAction::make(),
+                    Tables\Actions\EditAction::make(),
+                    Tables\Actions\DeleteAction::make(),
+                ]),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
