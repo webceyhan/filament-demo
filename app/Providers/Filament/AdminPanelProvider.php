829c9ba020987805819c96a7dc63a69a7cb23dea
Evren Ceyhan <evren.ceyhan@gmail.com>
2023-10-11 21:17:37 +0200

    breadcrumbs toggled

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -48,6 +48,7 @@ class AdminPanelProvider extends PanelProvider
                 'success' => Color::Emerald,
                 'warning' => Color::Orange,
             ])
+            // ->breadcrumbs(false) // disable breadcrumbs
             ->globalSearchKeyBindings(['command+k', 'ctrl+k'])
             ->sidebarCollapsibleOnDesktop() // collapse with icons
             // ->sidebarFullyCollapsibleOnDesktop() // full collapse without icons
