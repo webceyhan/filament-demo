5361979855ab1642db9347e5d93e80b0b46435b3
Evren Ceyhan <evren.ceyhan@gmail.com>
2023-10-11 19:13:55 +0200

    search key bindings added

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -46,6 +46,7 @@ class AdminPanelProvider extends PanelProvider
                 'success' => Color::Emerald,
                 'warning' => Color::Orange,
             ])
+            ->globalSearchKeyBindings(['command+k', 'ctrl+k'])            
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
