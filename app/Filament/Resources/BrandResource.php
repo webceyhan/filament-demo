8515911a842671fae0f453864c97c7f7e67e9cec
Evren Ceyhan <evren.ceyhan@gmail.com>
2023-10-11 20:10:10 +0200

    sidebar set to collapsible on desktop

--- app/Filament/Resources/BrandResource.php
+++ app/Filament/Resources/BrandResource.php
@@ -19,7 +19,7 @@ class BrandResource extends Resource
 {
     protected static ?string $model = Brand::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-building-office';
 
     protected static ?string $navigationGroup = 'Shop';
 
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -46,7 +46,9 @@ class AdminPanelProvider extends PanelProvider
                 'success' => Color::Emerald,
                 'warning' => Color::Orange,
             ])
-            ->globalSearchKeyBindings(['command+k', 'ctrl+k'])            
+            ->globalSearchKeyBindings(['command+k', 'ctrl+k'])
+            ->sidebarCollapsibleOnDesktop() // collapse with icons
+            // ->sidebarFullyCollapsibleOnDesktop() // full collapse without icons
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
