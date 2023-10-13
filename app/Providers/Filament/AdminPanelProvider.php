662f33987932db1698368f4070a546edf8c6a5d1
Evren Ceyhan <evren.ceyhan@gmail.com>
2023-10-13 19:14:33 +0200

    default dashboard widgets disabled

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -40,7 +40,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
-            ->profile()
+            ->profile()            
             ->colors([
                 'danger' => Color::Rose,
                 'gray' => Color::Gray,
@@ -77,8 +77,9 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
+                // disable default dashboard widgets
+                // Widgets\AccountWidget::class,
+                // Widgets\FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
