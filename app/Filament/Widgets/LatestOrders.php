0cd081d6822cf2c62c5bb438b999439a6290b330
Evren Ceyhan <evren.ceyhan@gmail.com>
2023-10-13 02:24:28 +0200

    latest order table widget added

---
+++ app/Filament/Widgets/LatestOrders.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Enums\OrderStatus;
+use App\Filament\Resources\OrderResource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Filament\Widgets\TableWidget as BaseWidget;
+
+class LatestOrders extends BaseWidget
+{
+    protected static ?int $sort = 4;
+
+    protected int|string|array $columnSpan = 'full';
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(OrderResource::getEloquentQuery())
+            ->defaultPaginationPageOption(5)
+            ->defaultSort('created_at', 'desc')
+            ->columns([
+                Tables\Columns\TextColumn::make('number')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('customer.name')
+                    ->sortable()
+                    ->searchable()
+                    ->toggleable(),
+                Tables\Columns\TextColumn::make('status')
+                    ->badge()
+                    ->color(fn (string $state) => OrderStatus::from($state)->color())
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->label('Date')
+                    ->date()
+                    ->sortable()
+            ]);
+    }
+}
