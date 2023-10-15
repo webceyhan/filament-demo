e17e67137dd6cb6f877d59553966ac1958e00d7d
Evren Ceyhan <evren.ceyhan@gmail.com>
2023-10-15 14:34:22 +0200

    user seeders moved to its own file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Artisan;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,40 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // reset cached roles and permissions
-        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
-
-        // generate permissions (clean, with policies, ignre prompts)
-        Artisan::call('permissions:sync --clean --policies --yes-to-all');
-
-        // generate roles
-        $adminRole = \Spatie\Permission\Models\Role::create(['name' => 'admin']);
-        $userRole = \Spatie\Permission\Models\Role::create(['name' => 'user']);
-
-        // give all permissions to admin role
-        $adminRole->givePermissionTo(\Spatie\Permission\Models\Permission::all());
-
-        // give all view permissions to user role
-        $userRole->givePermissionTo(
-            \Spatie\Permission\Models\Permission::query()
-                ->where('name', 'like', 'view%')
-                ->get()
-        );
-
-        // create admin user
-        \App\Models\User::factory()
-            ->create([
-                'name' => 'Joe Bloggs',
-                'email' => 'admin@demo.com',
-            ])
-            ->assignRole('admin');
-
-        // create normal users
-        \App\Models\User::factory(10)
-            ->create()
-            ->each(fn ($user) => $user->assignRole($userRole));
-
         $this->call([
+            UserSeeder::class,
             CategorySeeder::class,
             BrandSeeder::class,
             ProductSeeder::class,
